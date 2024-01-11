4d8f7fa2c7cb89c7125642ee8b2a60ce57089732
s8140156 <s8140156@gmail>
2024-01-11 10:57:40 +0800

    增加db.php, 增加back登入管理員判斷,調整css(button)及加上登入table

--- back.php
+++ back.php
@@ -1,3 +1,15 @@
+<?php include_once "./api/db.php";
+
+if(!empty($_POST)){
+  if($_POST['acc']=='admin' && $_POST['pw']=='1234'){
+    $_SESSION['login']=1;
+  }else{
+    $error="<div class='ct' style='color:red'>帳號或密碼錯誤</div>";
+  }
+}
+//這邊只有寫by session登入功能 沒有登出 若監評要看功能 要先關掉瀏覽器把session清除後再開啟 確認輸入錯誤的畫面=>登入畫面
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0055)?do=admin -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -6,7 +18,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title>影城</title>
   <link rel="stylesheet" href="css/css.css">
-  <link href="css/s2.css" rel="stylesheet" type="text/css">
+  <!-- <link href="css/s2.css" rel="stylesheet" type="text/css"> -->
   <script src="js/jquery-1.9.1.min.js"></script>
 </head>
 
@@ -22,6 +34,11 @@
       </marquee>
     </div>
     <div id="mm">
+      <!--這邊的判斷是看如果有收到session, 則可以看到以下畫面-->
+      <?php
+      if(isset($_SESSION['login'])){
+
+        ?>
       <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
         <a href="?do=tit">網站標題管理</a>|
         <a href="?do=go">動態文字管理</a>|
@@ -40,6 +57,35 @@
         }
         ?>
       </div>
+      <?php
+
+}else{
+  ?>
+      <!--這邊的判斷是看如果沒有收到session, 則可以看到以下畫面-->
+
+  <form action="" method="post" style="width:35%;margin:20px auto;">
+    <h3 class="ct">管理者登入</h3>
+    <?php
+    if(isset($error)){
+      echo $error;
+    }
+    ?>
+
+    <table>
+     <tr>
+       <td>帳號:</td>
+       <td><input type="text" name="acc" id=""></td>
+     </tr>
+     <tr>
+       <td>密碼:</td>
+       <td><input type="password" name="pw" id=""></td>
+     </tr>
+    </table>
+    <div class="ct"><input type="submit" value="登入"></div>
+  </form>
+ <?php
+}
+      ?>
     </div>
     <div id="bo"> ©Copyright 2010~2014 ABC影城 版權所有 </div>
   </div>
